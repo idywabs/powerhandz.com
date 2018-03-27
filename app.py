31a4853a3333d6886cfc2148b7f59e7a92aa68c8
Rahul Dhir <rahul.dhir96@gmail.com>
2018-03-27 02:22:15 -0500

    Other pages updated

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 @app.route('/')
 def index():
